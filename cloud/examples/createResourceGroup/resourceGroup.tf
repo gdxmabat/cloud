ebf6b2cf765dc5d070b313f8878c275745ef8f54
Mario Medina Bartolome <mmedinab@everis.com>
2018-11-22 13:49:13 +0100

    mensaje

--- cloud/examples/createResourceGroup/resourceGroup.tf
+++ cloud/examples/createResourceGroup/resourceGroup.tf
@@ -1,5 +1,10 @@
 provider "azurerm" {
+    subscription_id = "4c950822-b692-4f3a-a53f-40cc40aced2e"
+    client_id       = "8f6f1cfa-985d-4a0b-b247-93cc25d46ebf"
+    client_secret   = "52138f2c-ce4d-49fa-9108-f264cb16d405"
+    tenant_id       = "79add968-ae6e-433a-8aab-09abaa0fe1f6"
 }
+
 resource "azurerm_resource_group" "rg" {
         name = "gdxmabatResourceGroup"
         location = "westus"
